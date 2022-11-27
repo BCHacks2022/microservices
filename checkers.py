82c4c44f63a986ecb8b1efebbc413060ec203bf8
Murad Shahmammadli <64356412+MuradSh@users.noreply.github.com>
2022-11-27 12:35:43 -0800

    Missing clauses

--- checkers.py
+++ checkers.py
@@ -1,63 +1,84 @@
 def people(text):
-    if not "landlord" in text or not "tenant" in text:
-        return "The landlord or tenant field might be missing"
-
+    if not "landlord" in text:
+        return "landlord"
+    if not "tenant" in text:
+        return "tenant"
     return "Included"
 
+
 def starting_date(text):
-    synonyms = ["starting date","move in date", "starts on:"]
+    synonyms = ["starting date", "move in date", "starts on:"]
     for synonym in synonyms:
         if synonym in text:
             return "Included"
-    return "The starting date might be missing."
+    return "starting_date"
+
 
 def address(text):
     return "Included"
 
+
 def end_date(text):
-    synonyms = ["continues on a month-to-month basis", "continues on another periodic basis", "a fixed term ending on"]
+    synonyms = ["continues on a month-to-month basis",
+                "continues on another periodic basis", "a fixed term ending on"]
     for synonym in synonyms:
         if synonym in text:
             return "Included"
-    return "The ending date might be missing."
+    return "ending_date"
 
 
 def rent_price(text):
-    return "Did not check for rent price!"
+    synonyms = ["$", "pay the rent", "price"]
+    for synonym in synonyms:
+        if synonym in text:
+            return "Included"
+    return "rent_price"
+
 
 def pay_day(text):
-    return "Did not check for pay_day!"
+    synonyms = ["first day", "day of the month"]
+    for synonym in synonyms:
+        if synonym in text:
+            return "Included"
+    return "pay_day"
+
 
 def notice_before_rent_increase(text):
     synonyms = ["rent increase"]
     for synonym in synonyms:
         if synonym in text:
             return "Included"
-    return "The rent increase policy might be missing."
+    return "rent_increase"
+
 
 def security_deposit(text):
     if "security deposit" in text:
         return "Included"
-    return "The security deposit policy might be missing."
+    return "sec_dep"
+
 
 def smoking(text):
-    synonyms = ["smoking","cigarette","vape", "marijuana", "weed", "hemp", "cannabis"]
+    synonyms = ["smoking", "cigarette", "vape",
+                "marijuana", "weed", "hemp", "cannabis"]
     for synonym in synonyms:
         if synonym in text:
             return "Included"
-    return "The smoking policy might be missing."
+    return "smoking"
+
 
 def pet(text):
     if "pet" in text or "pets" in text:
         return "Included"
-    return "The pets policy might be missing."
+    return "pets"
+
 
 def utilities(text):
-    synonyms = ["utilities","water bills","electricity bills"]
+    synonyms = ["utilities", "water bills", "electricity bills"]
     for synonym in synonyms:
         if synonym in text:
             return "Included"
-    return "The utilities policy might be missing."
+    return "utilities"
 
 
-checkers = [people, end_date, starting_date, address, rent_price, pay_day, notice_before_rent_increase, security_deposit, smoking, pet, utilities]
+checkers = [people, end_date, starting_date, address, rent_price, pay_day,
+            notice_before_rent_increase, security_deposit, smoking, pet, utilities]
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+from checkers import checkers
+import numpy as np
+import pickle
+# import tensorflow as tf
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from PyPDF2 import PdfFileReader
 import io
 
 app = FastAPI()
+
+origins = [""]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -10,10 +24,9 @@ async def root():
     return {"message": "I am glad you are here, we have some cookies, come here!"}
 
 
-
-
 def analyzePDF(request_object_content):
-    parts=[]
+    parts = []
+
     def visitor_body(text, cm, tm, fontDict, fontSize):
         parts.append(text)
 
@@ -26,12 +39,12 @@ def analyzePDF(request_object_content):
     for i in parts:
         tmp = i.split(" ")
         for j in tmp:
-            if j!= "" and j!="\n":
-                word = ''.join(c for c in j.lower() if (48<=ord(c)<=57 or 97<=ord(c)<=122 or ord(c)==58))
+            if j != "" and j != "\n":
+                word = ''.join(c for c in j.lower() if (
+                    48 <= ord(c) <= 57 or 97 <= ord(c) <= 122 or ord(c) == 58))
                 words.append(word)
-    return [len(pages),words]
+    return [len(pages), words]
 
-from checkers import checkers
 
 @app.post("/contractKeywordChecking")
 async def contract(file: UploadFile):
@@ -43,37 +56,36 @@ async def contract(file: UploadFile):
     return [warning for warning in warnings if warning != "Included"]
 
 
-
-import tensorflow as tf
-import pickle
-import numpy as np
 with open("word_to_token.pkl", "rb") as f:
     word_to_token = pickle.load(f)
 
 max_length = 170
-@app.post("/pp")
-async def contract(text: str):
-    model1 = tf.keras.models.load_model('model1.h5')
-    model2 = tf.keras.models.load_model('model2.h5')
-    text = text.lower().split(". ")
-    frame = []
-    for i in text:
-        tmp = []
-        for j in i.split(" "):
-            try:
-                tmp.append(word_to_token[j])
-            except:
-                print("WWWWW")
-                tmp.append(0)
-        tmp.extend([0]*(max_length+1-len(tmp)))
-        # tmp.extend([0]*(max_length+1-len(i)))
-        frame.append(tmp)
-    print(len(frame[0]))
-    negative = np.array(model1.predict(np.array(frame)))
-    positive = np.array(model2.predict(np.array(frame)))
-    returns = []
-    print(text)
-    print(positive,negative)
-    for i in range(negative.shape[0]):
-        returns.append([text[i], "positive" if positive[i]>0.5 else "negative" if negative[i]>0.5 else "neutral"])
-    return returns
+
+
+# @app.post("/pp")
+# async def contract(text: str):
+#     model1 = tf.keras.models.load_model('model1.h5')
+#     model2 = tf.keras.models.load_model('model2.h5')
+#     text = text.lower().split(". ")
+#     frame = []
+#     for i in text:
+#         tmp = []
+#         for j in i.split(" "):
+#             try:
+#                 tmp.append(word_to_token[j])
+#             except:
+#                 print("WWWWW")
+#                 tmp.append(0)
+#         tmp.extend([0]*(max_length+1-len(tmp)))
+#         # tmp.extend([0]*(max_length+1-len(i)))
+#         frame.append(tmp)
+#     print(len(frame[0]))
+#     negative = np.array(model1.predict(np.array(frame)))
+#     positive = np.array(model2.predict(np.array(frame)))
+#     returns = []
+#     print(text)
+#     print(positive, negative)
+#     for i in range(negative.shape[0]):
+#         returns.append([text[i], "positive" if positive[i] >
+#                        0.5 else "negative" if negative[i] > 0.5 else "neutral"])
+#     return returns
