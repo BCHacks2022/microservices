cda19f71811408219f4354dc36055342278a0946
Wayne <wguo6358@gmail.com>
2022-11-26 16:18:03 -0800

    added text

---
+++ labeling.py
@@ -0,0 +1,10 @@
+with open("rawtext.txt","r") as f:
+    text = f.read().replace("\n"," ").split(".")
+
+
+
+for i in text:
+    print(i)
+    l = input("(Positive,Negative,Empty)>")
+    with open("data.txt","a") as f:
+        f.write(f"{i}------{l}")
