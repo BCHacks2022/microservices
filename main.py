3e4f1829a5167c56723372da0203ca8a997bae54
Murad Shahmammadli <64356412+MuradSh@users.noreply.github.com>
2022-11-27 13:57:41 -0800

    Back end changes

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
+# Get the models before running
+# https://drive.google.com/file/d/19B1fGVMn9-pSBemAbsl62L9tHawrjVe5/view?usp=drivesdk
+# https://drive.google.com/file/d/1-1g6ME8LA8FR2pPbgZ2md5wTr7BjkpM7/view?usp=drivesdk
 from checkers import checkers
+import requests
 import numpy as np
 import pickle
-# import tensorflow as tf
+import tensorflow as tf
 from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 from PyPDF2 import PdfFileReader
 import io
+from bs4 import BeautifulSoup
 
 app = FastAPI()
-
-origins = [""]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=[""],
-    allow_headers=["*"],
-)
 
 
 @app.get("/")
@@ -50,42 +45,99 @@ def analyzePDF(request_object_content):
 async def contract(file: UploadFile):
     request_object_content = await file.read()
     numberOfPages, text = analyzePDF(request_object_content)
-    print(text)
-    print("text")
+    print("landlord" in " ".join(text))
     warnings = [checker(" ".join(text)) for checker in checkers]
+    print(warnings)
     return [warning for warning in warnings if warning != "Included"]
 
+
+@app.post("/appendToPDF")
+async def contract(file: UploadFile, text: str):
+    request_object_content = await file.read()
 
 with open("word_to_token.pkl", "rb") as f:
     word_to_token = pickle.load(f)
 
 max_length = 170
+model1 = tf.keras.models.load_model('model1.h5')
+model2 = tf.keras.models.load_model('model2.h5')
 
 
-# @app.post("/pp")
-# async def contract(text: str):
-#     model1 = tf.keras.models.load_model('model1.h5')
-#     model2 = tf.keras.models.load_model('model2.h5')
-#     text = text.lower().split(". ")
-#     frame = []
-#     for i in text:
-#         tmp = []
-#         for j in i.split(" "):
-#             try:
-#                 tmp.append(word_to_token[j])
-#             except:
-#                 print("WWWWW")
-#                 tmp.append(0)
-#         tmp.extend([0]*(max_length+1-len(tmp)))
-#         # tmp.extend([0]*(max_length+1-len(i)))
-#         frame.append(tmp)
-#     print(len(frame[0]))
-#     negative = np.array(model1.predict(np.array(frame)))
-#     positive = np.array(model2.predict(np.array(frame)))
-#     returns = []
-#     print(text)
-#     print(positive, negative)
-#     for i in range(negative.shape[0]):
-#         returns.append([text[i], "positive" if positive[i] >
-#                        0.5 else "negative" if negative[i] > 0.5 else "neutral"])
-#     return returns
+@app.post("/pp")
+def ppML(text):
+    text = text.lower().split(". ")
+    frame = []
+    for i in text:
+        tmp = []
+        for j in i.split(" "):
+            try:
+                tmp.append(word_to_token[j])
+            except:
+                # print("WWWWW")
+                tmp.append(0)
+        if len(tmp) >= max_length:
+            print("PP")
+            print(len(tmp[0:max_length+1]))
+            frame.append(tmp[0:max_length+1])
+
+            # for i in range(0,len(tmp),max_length+1):
+            #     frame.append(tmp[i:i+max_length+1])
+        else:
+            tmp.extend([0]*(max_length+1-len(tmp)))
+            frame.append(tmp)
+
+            # print(len(tmp))
+        # tmp.extend([0]*(max_length+1-len(i)))
+    print(len(frame[10]))
+    frame = np.array(frame)
+    # for i in frame:
+    #     print(len(frame))
+    negative = np.array(model1.predict(np.array(frame)))
+    positive = np.array(model2.predict(np.array(frame)))
+    returns = []
+    print(text)
+    print(positive, negative)
+    for i in range(negative.shape[0]):
+        returns.append([text[i], "positive" if positive[i] >
+                       0.5 else "negative" if negative[i] > 0.5 else "neutral"])
+    return returns
+
+
+async def contract(text: str):
+    return ppML(text)
+
+
+@app.post("/ppURL")
+async def contract2(url: str):
+    if url == " ":
+        url = "https://www.linkedin.com/legal/privacy-policy"
+    # From https://matix.io/extract-text-from-webpage-using-beautifulsoup-and-python/
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, "html.parser")
+    text = soup.find_all(text=True)
+    output = ''
+    blacklist = [
+        '[document]',
+        'noscript',
+        'header',
+        'html',
+        'meta',
+        'head',
+        'input',
+        'script',
+        'style',
+        'h1',
+        'h2',
+        'h3',
+        'iframe',
+        'video'
+        # there may be more elements you don't want, such as "style", etc.
+    ]
+
+    for t in text:
+        if t.parent.name not in blacklist:
+            output += '{} '.format(t)
+
+    # End from
+
+    return ppML(output)
